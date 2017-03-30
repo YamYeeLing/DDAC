b0516db595a6a52defbb603980527bc7749e302c
YamYeeLing <TP034123@mail.apu.edu.my>
2017-03-31 07:10:05 +0800

    DDAC updated

---
+++ DDAC/Controllers/CargoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDAC.Models;
+
+namespace DDAC.Controllers
+{
+    public class CargoesController : Controller
+    {
+        private DDACEntities5 db = new DDACEntities5();
+
+        // GET: Cargoes
+        public ActionResult Index()
+        {
+            var cargoes = db.Cargoes.Include(c => c.Customer).Include(c => c.Warehouse);
+            return View(cargoes.ToList());
+        }
+
+        // GET: Cargoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name");
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "WarehouseId", "WarehouseName");
+            return View();
+        }
+
+        // POST: Cargoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CargoName,CargoWidth,CargoLength,CargoHeight,CargoWeight,CustomerId,WarehouseId,CargoRequirement")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cargoes.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "WarehouseId", "WarehouseName", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "WarehouseId", "WarehouseName", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // POST: Cargoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CargoName,CargoWidth,CargoLength,CargoHeight,CargoWeight,CustomerId,WarehouseId,CargoRequirement")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "WarehouseId", "WarehouseName", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cargo cargo = db.Cargoes.Find(id);
+            db.Cargoes.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DDAC/Controllers/ShiptransactionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DDAC.Models;
+
+namespace DDAC.Controllers
+{
+    public class ShiptransactionsController : Controller
+    {
+        private DDACEntities5 db = new DDACEntities5();
+
+        // GET: Shiptransactions
+        public ActionResult Index()
+        {
+            return View(db.Shiptransactions.ToList());
+        }
+
+        // GET: Shiptransactions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shiptransaction shiptransaction = db.Shiptransactions.Find(id);
+            if (shiptransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shiptransaction);
+        }
+
+        // GET: Shiptransactions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shiptransactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipId,ShipName,DepartLocation,DestinationLocation")] Shiptransaction shiptransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shiptransactions.Add(shiptransaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shiptransaction);
+        }
+
+        // GET: Shiptransactions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shiptransaction shiptransaction = db.Shiptransactions.Find(id);
+            if (shiptransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shiptransaction);
+        }
+
+        // POST: Shiptransactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShipId,ShipName,DepartLocation,DestinationLocation")] Shiptransaction shiptransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shiptransaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shiptransaction);
+        }
+
+        // GET: Shiptransactions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shiptransaction shiptransaction = db.Shiptransactions.Find(id);
+            if (shiptransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shiptransaction);
+        }
+
+        // POST: Shiptransactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shiptransaction shiptransaction = db.Shiptransactions.Find(id);
+            db.Shiptransactions.Remove(shiptransaction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
